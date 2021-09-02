6b127502960171f78d8a5b94d65ee80dd160baf3
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2021-09-02 20:07:26 -0300

    Agreaga y Muestra categorias {Arreglar para que traiga cuando cargue la pagina

--- client/backend/js/backend.js
+++ client/backend/js/backend.js
@@ -1,13 +1,31 @@
 // ----Variables----//
 let formAgregar = document.getElementById('agregarProductos');
-let inputs = document.querySelectorAll('input');
-let textarea = document.querySelector('textarea').value;
-let select = document.getElementById('selectAgregar').value;
+let formCategoria = document.getElementById('agregarCategoria');
+let inputs = formAgregar.querySelectorAll('input');
+let inputCategoria = formCategoria.querySelector('input');
+let textarea = formAgregar.querySelector('textarea').value;
+let select = document.getElementById('selectAgregar');
 let errorDiv = document.querySelector('.error');
+let errorCategoria = document.querySelector('.errorCategoria');
+var myModal = document.getElementById('exampleModal')
+var myInput = document.getElementById('myInput')
 var validacion = false;
 // ----Variables----//
 // -----------------//
 // ----Funciones----//
+const mostrarCategorias = ()=>{
+    fetch("http://localhost/e-commerce-ferreyra/boilerplate_front_back_php/server/mostrar-categorias")
+    .then(res=>res.json())
+    .then (async categoria=>{
+        console.log(categoria);
+        for (let i = 0; i < categoria.length; i++) {
+            const option = document.createElement('option');
+            option.setAttribute("value", + categoria[i].id_categoria);
+            option.innerText = categoria[i].nombre;
+            select.appendChild(option);
+        }
+    });
+}
 const agregarProducto = (e)=>{
     for (let i = 0; i < inputs.length; i++) {
         validacion = false;
@@ -38,6 +56,32 @@ const agregarProducto = (e)=>{
         for (let i = 0; i < inputs.length; i++) {inputs[i].value = ""}
    }
 }
+const agregarCategoria = ()=>{
+    validacion = false;
+        if (inputCategoria.value == "") {
+                errorCategoria.classList.add('alert');
+                errorCategoria.classList.add('alert-danger');
+                errorCategoria.innerHTML = "!!Completar los Campos¡¡";
+        }
+        else{
+            validacion = true;
+        }
+        if (validacion) {
+            let envioCategoria = new FormData(formCategoria);
+            fetch("http://localhost/e-commerce-ferreyra/boilerplate_front_back_php/server/agregar-categoria",
+            {
+                method: "POST",
+                body: envioCategoria
+            }).then(res=>res.json())
+            .then(Response=>{
+                errorCategoria.classList.add('alert');
+                errorCategoria.classList.remove('alert-danger');
+                errorCategoria.classList.add('alert-success');
+                errorCategoria.innerHTML = Response;
+                console.log(Response);  
+            });
+        }
+}
 // ----Funciones----//
 // -----------------//
 // ----Eventos----//
@@ -45,4 +89,12 @@ formAgregar.addEventListener('submit', (e)=>{
     e.preventDefault();
     agregarProducto();
 });
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+});
+formCategoria.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    agregarCategoria();
+});
+let section = document.querySelector('section').addEventListener('click', mostrarCategorias)
 // ----Eventos----//
