3b865a0e384afa7d8f941f86191fd119bfab4aad
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2021-09-06 21:32:25 -0300

    Eliminar un producto completado

--- server/routes/carrito.php
+++ server/routes/carrito.php
@@ -3,7 +3,7 @@ router("GET","/carrito/:id_usuario", function($id_usuario){
     include_once("./models/carrito.php");
     echo mostrar_carrito($id_usuario);
 });
-router("POST", "/eliminar-carrito/:id_carrito", function($id_carrito){
+router("POST", "/eliminar-carrito", function($id_carrito){
     include_once("./models/carrito.php");
     echo eliminar_carrito($id_carrito);
 });
