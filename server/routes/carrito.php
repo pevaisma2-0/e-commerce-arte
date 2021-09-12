495cf1c83f49cb7167b65078bdd0773eb6c013c1
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2021-09-12 18:48:48 -0300

    carrito ABM terminado

--- server/routes/carrito.php
+++ server/routes/carrito.php
@@ -3,7 +3,14 @@ router("GET","/carrito/:id_usuario", function($id_usuario){
     include_once("./models/carrito.php");
     echo mostrar_carrito($id_usuario);
 });
-router("POST", "/eliminar-carrito", function($id_carrito){
+router("POST", "/agregar-carrito", function (){
+    include_once("./models/carrito.php");
+    $id_carrito = new stdClass();
+    $id_carrito->producto = $_POST['id_producto'];
+    $id_carrito->usuario = $_POST['id_usuario'];
+    echo agregar_carrito($id_carrito);
+});
+router("POST", "/eliminar-carrito/:id_carrito", function($id_carrito){
     include_once("./models/carrito.php");
     echo eliminar_carrito($id_carrito);
 });
