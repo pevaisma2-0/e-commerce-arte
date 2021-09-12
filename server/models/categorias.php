cd990b15d197eb65216a97b84b3776bf27d8379b
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2021-09-12 19:21:09 -0300

    modificar categorias: completado

--- server/models/categorias.php
+++ server/models/categorias.php
@@ -10,3 +10,8 @@ function mostrar_categorias(){
     $registos = $consulta->fetch_all(1);
     return json_encode($registos);
 }
+function modificar_categorias($modificar){
+    include_once("./conexion.php");
+    $consulta = $conexion->query("UPDATE categorias SET nombre ='$modificar->categoria' WHERE id_categoria =".$modificar->id_categoria);
+    return json_encode("El nombre de la categoria es ahora: ".$modificar->categoria);
+}
